2a3d5a5609742fe89ae98ce57057a4a67958067b
Jeremie DECOCK <jd.jdhp@gmail.com>
2016-04-02 19:30:13 +0200

    Add some modules.

---
+++ tktools/png.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2016 Jérémie DECOCK (http://www.jdhp.org)
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+ 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+"""
+TODO...
+"""
+
+__all__ = ['check_compatibility']
+
+import tkinter as tk
+
+def check_compatibility():
+    """
+    TODO...
+    """
+
+    if tk.TkVersion < 8.6:
+        print("*" * 80)
+        print("WARNING: Tk version {} is installed on your system.".format(tk.TkVersion))
+        print("Tk < 8.6 only supports three file formats: GIF, PGM and PPM.")
+        print("You need to install Tk >= 8.6 if you want to read JPEG and PNG images!")
+        print("*" * 80)
+
+        return False
+    else:
+        return True
+
